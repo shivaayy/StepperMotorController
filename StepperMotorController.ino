5ea34b97677fa19a0f99a93a74f5d21ab2e9f4c5
shivaayy <45315896+shivaayy@users.noreply.github.com>
2021-01-16 23:05:27 +0800

    test 1

--- StepperMotorController.ino
+++ StepperMotorController.ino
@@ -2,30 +2,39 @@
 int dir_pin=3;
 int pul_pin=2;
 int temp=10000;
+float z=0.0;
+int potentioMeterReading=0;
 void setup()
 {
   pinMode(dir_pin,OUTPUT);
   pinMode(pul_pin,OUTPUT);
   digitalWrite(dir_pin,HIGH);
+  Serial.begin(9600);
   
-  for(int j=0;j<19;j++){
+  for(int j=1;j<=18;j++){
+    Serial.println(j);
+    z=150000.0/temp;
+    Serial.println(z);
     
-    for(int i=0;i<200*3;i++){
-  digitalWrite(pul_pin,HIGH);
- delayMicroseconds(temp);
- digitalWrite(pul_pin,LOW);
- delayMicroseconds(temp);
+    for(int i=0;i<200*1;i++){
+      digitalWrite(pul_pin,HIGH);
+      delayMicroseconds(temp);
+      digitalWrite(pul_pin,LOW);
+      delayMicroseconds(temp);
   
  }
- temp=temp-400;
+ 
+    temp=temp-400;
     
   }
-
-for(int i=0;i<200*30;i++){
+  temp=temp-100; 
+  z=150000.0/temp;
+  Serial.println(z);
+  for(int i=0;i<200*30;i++){
   digitalWrite(pul_pin,HIGH);
- delayMicroseconds(temp);
- digitalWrite(pul_pin,LOW);
- delayMicroseconds(temp);
+  delayMicroseconds(temp);
+  digitalWrite(pul_pin,LOW);
+  delayMicroseconds(temp);
   
  }
    
@@ -34,12 +43,17 @@ for(int i=0;i<200*30;i++){
 
 void loop()
 {
-temp=4000;
-  for(int i=0;i<200*30;i++){
-  digitalWrite(pul_pin,HIGH);
- delayMicroseconds(temp);
- digitalWrite(pul_pin,LOW);
- delayMicroseconds(temp);
+  potentioMeterReading=analogRead(A0);
+  temp = map(potentioMeterReading,0,1024,4500,2500);
+  
+  
+  Serial.println(150000.0/temp);
+  
+  for(int i=0;i<200;i++){
+    digitalWrite(pul_pin,HIGH);
+    delayMicroseconds(temp);
+    digitalWrite(pul_pin,LOW);
+    delayMicroseconds(temp);
   
  }
 
