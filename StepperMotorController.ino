43e101f40a8e0c647e98f02d3e2ffc036938142d
shivaayy <45315896+shivaayy@users.noreply.github.com>
2021-01-19 19:51:37 +0800

    startind speed changed

--- StepperMotorController.ino
+++ StepperMotorController.ino
@@ -1,7 +1,7 @@
 
 int dir_pin=3;
 int pul_pin=2;
-int temp=10000;
+int temp=15200;
 float z=0.0;
 int potentioMeterReading=0;
 void setup()
@@ -11,14 +11,14 @@ void setup()
   digitalWrite(dir_pin,HIGH);
   Serial.begin(9600);
   
-  for(int j=1;j<=18;j++){
-    Serial.println(j);
-    z=150000.0/temp;
+  for(int j=1;j<=35;j++){
+//    Serial.println(j);
+    z=75000.0/temp;
     Serial.println(z);
     
-    for(int i=0;i<200*1;i++){
+    for(int i=0;i<200*4;i++){
       digitalWrite(pul_pin,HIGH);
-      delayMicroseconds(temp);
+//      delayMicroseconds(temp);
       digitalWrite(pul_pin,LOW);
       delayMicroseconds(temp);
   
@@ -27,12 +27,12 @@ void setup()
     temp=temp-400;
     
   }
-  temp=temp-100; 
-  z=150000.0/temp;
+  temp=temp+100; 
+  z=75000.0/temp;
   Serial.println(z);
-  for(int i=0;i<200*30;i++){
+  for(int i=0;i<200*40;i++){
   digitalWrite(pul_pin,HIGH);
-  delayMicroseconds(temp);
+//  delayMicroseconds(temp);
   digitalWrite(pul_pin,LOW);
   delayMicroseconds(temp);
   
@@ -44,14 +44,14 @@ void setup()
 void loop()
 {
   potentioMeterReading=analogRead(A0);
-  temp = map(potentioMeterReading,0,1024,4500,2500);
+  temp = map(potentioMeterReading,0,1024,2300,1200);
   
   
-  Serial.println(150000.0/temp);
+  Serial.println(75000.0/temp);
   
-  for(int i=0;i<200;i++){
+  for(int i=0;i<200*4;i++){
     digitalWrite(pul_pin,HIGH);
-    delayMicroseconds(temp);
+//    delayMicroseconds(temp);
     digitalWrite(pul_pin,LOW);
     delayMicroseconds(temp);
   
