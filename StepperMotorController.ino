89da05f93e1cedef48978f2a9f91d3d3ac4b3b17
shivaayy <45315896+shivaayy@users.noreply.github.com>
2021-12-19 23:42:58 +0530

    calibrated for max load

--- StepperMotorController.ino
+++ StepperMotorController.ino
@@ -1,22 +1,23 @@
 
 int dir_pin=3;
 int pul_pin=2;
-int temp=2650;
+int temp=14050;
 float z=0.0;
 int potentioMeterReading=0;
 void setup()
 {
   pinMode(dir_pin,OUTPUT);
   pinMode(pul_pin,OUTPUT);
-  digitalWrite(dir_pin,HIGH);
+  digitalWrite(dir_pin,LOW);
   Serial.begin(9600);
-  
-  for(int j=1;j<=3;j++){
-    Serial.println(j);
-    z=150000.0/temp;
-    Serial.println(z);
+
+    for(int j=1;j<=17;j++){
+//    Serial.println(j);
+//    z=150000.0/temp;
+//    Serial.println(z);
     
-    for(int i=0;i<200*24;i++){
+    for(int i=0;i<200
+    ;i++){
       digitalWrite(pul_pin,HIGH);
       delayMicroseconds(temp);
       digitalWrite(pul_pin,LOW);
@@ -27,9 +28,48 @@ void setup()
     temp=temp-600;
     
   }
-  temp=temp-100; 
+  
+  for(int j=1;j<=3;j++){
+//    Serial.println(j);
+//    z=150000.0/temp;
+//    Serial.println(z);
+    
+    for(int i=0;i<200*21
+
+
+    
+    ;i++){
+      digitalWrite(pul_pin,HIGH);
+      delayMicroseconds(temp);
+      digitalWrite(pul_pin,LOW);
+      delayMicroseconds(temp);
+  
+ }
+ 
+    temp=temp-600;
+    
+  }
+
+    for(int j=1;j<=2;j++){
+//    Serial.println(j);
+//    z=150000.0/temp;
+//    Serial.println(z);
+    
+    for(int i=0;i<200*24
+    ;i++){
+      digitalWrite(pul_pin,HIGH);
+      delayMicroseconds(temp);
+      digitalWrite(pul_pin,LOW);
+      delayMicroseconds(temp);
+  
+ }
+ 
+    temp=temp-600;
+    
+  }
+  temp=temp+100; 
   z=150000.0/temp;
-  Serial.println(z);
+//  Serial.println(z);
   for(int i=0;i<200*30;i++){
   digitalWrite(pul_pin,HIGH);
   delayMicroseconds(temp);
@@ -44,12 +84,12 @@ void setup()
 void loop()
 {
   potentioMeterReading=analogRead(A0);
-  temp = map(potentioMeterReading,0,1024,1500,750);
+  temp = map(potentioMeterReading,0,1024,1200,800);
   
   
-  Serial.println(150000.0/temp);
+//  Serial.println(150000.0/temp);
   
-  for(int i=0;i<200*3;i++){
+  for(int i=0;i<200*15;i++){
     digitalWrite(pul_pin,HIGH);
     delayMicroseconds(temp);
     digitalWrite(pul_pin,LOW);
