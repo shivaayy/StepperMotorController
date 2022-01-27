925ca1f1273bdca02c99be54c35aeb178df2c571
shivaayy <45315896+shivaayy@users.noreply.github.com>
2022-01-27 20:17:16 +0530

    velocity formula modified

--- StepperMotorController.ino
+++ StepperMotorController.ino
@@ -4,7 +4,7 @@ long int maximumSpeed=4000;
 int count=0;
 // stepper control global variable
 int starting_delay_microseconds = 10000;
-int final_delay_microseconds_at_max_speed = 100;
+int final_delay_microseconds_at_max_speed = 125;
 int current_delay_microseconds=starting_delay_microseconds;
 unsigned long int start_millis=0;
 unsigned long int current_millis=0;
@@ -12,7 +12,7 @@ unsigned long int setup_time_in_sec=0;
 /////////
 
 //
-float acceleration =1;  // steps/sec^2 
+float acceleration =30;  // steps/sec^2 
 //
 void setup() {
   Serial.begin(9600);
@@ -45,14 +45,15 @@ if(setup_mode && current_delay_microseconds>final_delay_microseconds_at_max_spee
 //Serial.println(current_delay_microseconds);
   current_millis=temp_millis;
   setup_time_in_sec++;
-  current_delay_microseconds=500000.0*current_delay_microseconds/(500000+current_delay_microseconds*acceleration*setup_time_in_sec);
+  current_delay_microseconds=500000.0*starting_delay_microseconds/(500000+starting_delay_microseconds*acceleration*setup_time_in_sec);
 }
 else{
   if(temp_millis-current_millis>=1000){
-//      int temp_read= map(analogRead(A0),0,1023,300,130);
+//      int temp_read= map(analogRead(A0),0,1023,150,124);
 //      if(temp_read-final_delay_microseconds_at_max_speed>1 || temp_read-final_delay_microseconds_at_max_speed<-1){
 //        setup_time_in_sec=0;
 //        final_delay_microseconds_at_max_speed=temp_read;
+//        starting_delay_microseconds=current_delay_microseconds;
 //        }
   }
   if( current_delay_microseconds>final_delay_microseconds_at_max_speed && temp_millis-current_millis>=1000){
@@ -60,14 +61,14 @@ else{
 //Serial.println(current_delay_microseconds);
   current_millis=temp_millis;
   setup_time_in_sec++;
-  current_delay_microseconds=500000.0*current_delay_microseconds/(500000+current_delay_microseconds*acceleration*setup_time_in_sec);
+  current_delay_microseconds=500000.0*starting_delay_microseconds/(500000+starting_delay_microseconds*acceleration*setup_time_in_sec);
 }
   else if(current_delay_microseconds-final_delay_microseconds_at_max_speed<-1 && temp_millis-current_millis>=1000){
 //    Serial.println("dis-accelarting");
 //Serial.println(current_delay_microseconds);
   current_millis=temp_millis;
   setup_time_in_sec++;
-  current_delay_microseconds=500000.0*current_delay_microseconds/(500000-current_delay_microseconds*acceleration*setup_time_in_sec);
+  current_delay_microseconds=500000.0*starting_delay_microseconds/(500000-starting_delay_microseconds*acceleration*setup_time_in_sec);
 }
 
   }
